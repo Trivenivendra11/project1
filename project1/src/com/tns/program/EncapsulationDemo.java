e3e3c1c9ffe80ef60a061d527d7a8eccd993e84c
svkp <svkp@10.0.0.145>
2023-09-27 14:47:38 +0530

    Second Commit

---
+++ project1/src/com/tns/program/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tns.program;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encapsulation ob=new Encapsulation();// created object
+ob.serialno=33;
+ob.name="triveni";
+ob.age=22;
+ob.show();
+	}
+
+}
