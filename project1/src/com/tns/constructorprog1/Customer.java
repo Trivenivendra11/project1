6b3f217fb841eab21e27dba88e5f8c5dd4ea180b
svkp <svkp@10.0.0.145>
2023-10-05 12:26:32 +0530

    third commit

---
+++ project1/src/com/tns/constructorprog1/Customer.java
@@ -0,0 +1,43 @@
+package com.tns.constructorprog1;
+
+public class Customer {
+
+	private String customerName;
+	private int customerId;
+	private String customerCity;
+	
+	public Customer()
+	{
+	}
+	//parameterized constructor
+	public Customer (String customerName,int customerId,String customerCity) {
+	
+		this.customerName=customerName;
+		this.customerId=customerId;
+		this.customerCity=customerCity;
+	}
+	
+		public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerCity() {
+		return customerCity;
+	}
+	public void setCustomerCity(String customerCity) {
+		this.customerCity = customerCity;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerName=" + customerName + ", customerId=" + customerId + ", customerCity="
+				+ customerCity + "]";
+	}
+}
